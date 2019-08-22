1422e3b8c7b74007e4ee6c96ced5be18c80ff522
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2019-08-23 00:42:35 +0900

    Add GNUGo module as download and build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+git submodule init
+git submodule update
+
+# download GNUGo
+pushd modules
+if [[ ! -e ./gnugo-3.8.tar.gz ]]; then
+  wget http://ftp.gnu.org/gnu/gnugo/gnugo-3.8.tar.gz
+  tar -xzvf gnugo-3.8.tar.gz
+
+  pushd gnugo-3.8
+  ./configure --build=i686-pc-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
+  make -j4
+  popd
+
+  cp gnugo-3.8/interface/gnugo .
+fi
+
+popd
+
+make -j4
