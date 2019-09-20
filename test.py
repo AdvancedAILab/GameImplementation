fef4d1167041e94ee2eda8fc36a7caef3d3277d5
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2019-09-20 20:27:30 +0900

    Update simple test

--- test.py
+++ test.py
@@ -5,10 +5,11 @@ classes = [
     'Reversi',
     'AnimalShogi',
     'Go',
+    'Geister',
 ]
 
 for game in classes:
     s = getattr(games, game)()
     print(s)
     print(s.legal_actions())
-
+    print([s.action2str(a) for a in s.legal_actions()])
